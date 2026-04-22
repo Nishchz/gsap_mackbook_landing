36683f3c6cdf5a98fee62596286b4f295d2146a2
Nishchz <anishchz01@gmail.com>
2026-04-22 22:53:49 +0530

    Implement 3d Product Viewer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from './Components/NavBar'
 import Hero from './Components/Hero'
+import ProductViewer from './Components/ProductViewer'
+import { ScrollTrigger ,SplitText } from 'gsap/all'
+import gsap from 'gsap'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
   return (
     <main>
         <NavBar/>
         <Hero/>
+        <ProductViewer/>
     </main>
   )
 }
