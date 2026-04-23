7e9756f604a0a6254b2556c810ef61705a6c5ba4
Nishchz <anishchz01@gmail.com>
2026-04-23 23:48:04 +0530

    implement footer section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Showcase from './Components/Showcase';
 import Performance from './Components/Performance';
 import Features from './Components/Features.jsx';
 import Highlights from './Components/Highlights.jsx';
+import Footer from './Components/Footer.jsx';
 
 
 
@@ -23,6 +24,7 @@ const App = () => {
         <Performance/>
         <Features/>
         <Highlights/>
+        <Footer/>
         
       
      
